2fa948272a8c115da46c7614fbab62104d6d9789
Pedro Henrique de Miranda Arthur <pedro@aceelectronix.com>
2018-06-22 09:58:54 -0700

    bean post processors

--- src/com/spring_java/HelloWorld.java
+++ src/com/spring_java/HelloWorld.java
@@ -2,12 +2,20 @@ package com.spring_java;
 
 public class HelloWorld {
 	private String message;
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
+
 	public void getMessage() {
 		System.out.println("Your message: " + message);
+	}
+
+	public void init(){
+		System.out.println("Bean is going through init.");
+	}
+	
+	public void destroy() {
+		System.out.println("Bean will destroy now.");
 	}
 }
--- src/com/spring_java/MainApp.java
+++ src/com/spring_java/MainApp.java
@@ -1,20 +1,18 @@
 package com.spring_java;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-
-		objA.setMessage("I'm object A");
-		objA.getMessage();
-
-		HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-		objB.getMessage();
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
 	}
 
 }
