2975fe580f2e7cd4e0efb84aef57d8229be09050
Pedro Henrique de Miranda Arthur <pedro@aceelectronix.com>
2018-06-22 09:45:30 -0700

    testing singleton

--- src/com/spring_java/MainApp.java
+++ src/com/spring_java/MainApp.java
@@ -8,8 +8,13 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		obj.getMessage();
+		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+		objA.setMessage("I'm object A");
+		objA.getMessage();
+
+		HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+		objB.getMessage();
 	}
 
 }
